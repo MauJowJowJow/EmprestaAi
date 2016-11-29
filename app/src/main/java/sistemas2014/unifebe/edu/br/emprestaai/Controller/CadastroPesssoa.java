bd3c150b3c7efcdc0cff5c345db728c1003ce942
DougPinho <douglas.dpinho@hotmail.com>
2016-11-29 21:11:10 -0200

    Cadastro pessoa

--- app/src/main/java/sistemas2014/unifebe/edu/br/emprestaai/Controller/CadastroPesssoa.java
+++ app/src/main/java/sistemas2014/unifebe/edu/br/emprestaai/Controller/CadastroPesssoa.java
@@ -1,12 +1,13 @@
 package sistemas2014.unifebe.edu.br.emprestaai.Controller;
 
-import android.content.ContentValues;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import sistemas2014.unifebe.edu.br.emprestaai.Model.Pessoa;
 
 import sistemas2014.unifebe.edu.br.emprestaai.R;
 
@@ -26,7 +27,37 @@ public class CadastroPesssoa extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                EditText nome = (EditText) findViewById(R.id.txtNome);
+                EditText txtNome = (EditText) findViewById(R.id.txtNome);
+                EditText txtBairro = (EditText) findViewById(R.id.txtBairro);
+                EditText txtCep = (EditText) findViewById(R.id.txtCEP);
+                EditText txtCidade = (EditText) findViewById(R.id.txtCidade);
+                EditText txtEstado = (EditText) findViewById(R.id.txtEstado);
+                EditText txtRua = (EditText) findViewById(R.id.txtRua);
+                EditText txtTelefone = (EditText) findViewById(R.id.txtTelefone);
+                EditText txtEmail = (EditText) findViewById(R.id.txtEmail);
+
+                Pessoa pessoa = new Pessoa();
+
+                pessoa.setNome(txtNome.getText().toString());
+                pessoa.setBairro(txtBairro.getText().toString());
+                pessoa.setCep(txtCep.getText().toString());
+                pessoa.setCidade(txtCidade.getText().toString());
+                pessoa.setEstado(txtEstado.getText().toString());
+                pessoa.setRua(txtRua.getText().toString());
+                pessoa.setTelefone(txtTelefone.getText().toString());
+                pessoa.setEmail(txtEmail.getText().toString());
+
+
+                pessoa.save();
+
+                if (pessoa.getId() != 0){
+                    Toast.makeText(getApplicationContext(), "Cadastro salvo com sucesso!", Toast.LENGTH_LONG).show();
+                    finish();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Erro no cadastro!", Toast.LENGTH_LONG).show();
+                }
+
+
 
             }
         });
