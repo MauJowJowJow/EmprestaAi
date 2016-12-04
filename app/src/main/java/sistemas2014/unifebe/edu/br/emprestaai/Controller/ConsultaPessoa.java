640606242539266da018c95d6a44b1ace1c73990
MauJowJowJow <maumau.g.o@hotmail.com>
2016-12-04 17:14:47 -0200

    Consulta Pessoa

--- app/src/main/java/sistemas2014/unifebe/edu/br/emprestaai/Controller/ConsultaPessoa.java
+++ app/src/main/java/sistemas2014/unifebe/edu/br/emprestaai/Controller/ConsultaPessoa.java
@@ -2,10 +2,14 @@ package sistemas2014.unifebe.edu.br.emprestaai.Controller;
 
 import android.app.ListActivity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import sistemas2014.unifebe.edu.br.emprestaai.Model.Pessoa;
 import sistemas2014.unifebe.edu.br.emprestaai.R;
@@ -16,24 +20,47 @@ import sistemas2014.unifebe.edu.br.emprestaai.R;
 
 public class ConsultaPessoa extends ListActivity {
 
+    private List<Pessoa> listaPessoas =
+            new ArrayList<Pessoa>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_consulta_pessoa);
 
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>
+                (this,android.R.layout.simple_list_item_1,
+                        getListaPessoas());
+        setListAdapter(dataAdapter);
 
+    }
 
+    @Override
+    protected void onListItemClick(ListView l, View v,
+                                   int position, long id) {
+        super.onListItemClick(l, v, position, id);
 
+        /* Fazer tela de detalhes da Pessoa recebendo o ID por parametro e carregando os dados.
 
-      //  //List <Pessoa> pessoas = Pessoa.listAll(Pessoa.class);
+        Intent i = new Intent(getApplicationContext(), DetalhePessoa.class);
+        i.putExtra("id", listaPessoas.get(position).getId());
+        startActivity(i);*/
+    }
 
-       // List<Pessoa> pessoas = Pessoa.findWithQuery(Pessoa.class,"Select nome from pessoa");
+    private List<String> getListaPessoas(){
+        Iterator<Pessoa> listDB = Pessoa.findAll(Pessoa.class);
+        List<String> listaNome = new ArrayList<String>();
+        listaPessoas = new ArrayList<Pessoa>();
 
-      //  list.setAdapter(new ArrayAdapter<Pessoa>(this, android.R.layout.simple_list_item_1, pessoas));
+        Pessoa pessoa;
+        while (listDB.hasNext()) {
+            pessoa = listDB.next();
+            listaNome.add(pessoa.getNome());
+            listaPessoas.add(pessoa);
+        }
 
-
-
-
+        return listaNome;
     }
 }
 
